f1532cacdd79e386d03dec18cab62214e70b1144
Tana <finaltruth.shintai@gmail.com>
2022-12-26 01:53:09 +0900

    タイプ固定用のバトルページ効果を追加。

---
+++ Src/PokeAbilities/Types/DiceCardSelfAbilityTypeBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PokeAbilities.Types
+{
+    /// <summary>
+    /// 固定されたタイプを持たせるためのバトル ページ効果の基底クラスです。
+    /// </summary>
+    public abstract class DiceCardSelfAbilityTypeBase : DiceCardSelfAbilityBase
+    {
+        /// <summary>
+        /// バトル ページに固定で付与させるタイプを取得します。
+        /// </summary>
+        public abstract PokeType Type { get; }
+
+        public sealed override string[] Keywords => base.Keywords;
+
+        public override void OnAddToHand(BattleUnitModel owner)
+        {
+            try
+            {
+                card.card.TryAddType(Type, true);
+            }
+            catch (Exception ex)
+            {
+                Log.Instance.ErrorOnExceptionThrown(ex);
+            }
+        }
+    }
+}
---
+++ Src/PokeAbilities/Types/TypeCardAbilities/DiceCardSelfAbility_water.cs
@@ -0,0 +1,10 @@
+﻿namespace PokeAbilities.Types.TypeCardAbilities
+{
+    /// <summary>
+    /// バトル ページ効果「このページはみずタイプ固定」
+    /// </summary>
+    public class DiceCardSelfAbility_water : DiceCardSelfAbilityTypeBase
+    {
+        public override PokeType Type => PokeType.Water;
+    }
+}
